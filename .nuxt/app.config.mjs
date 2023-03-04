86be46e4b2dcd827c984f4060067ef8eb2d58930
Anucha Buatum <anucha_bua@jobbkk.co.th>
2023-03-04 15:46:13 +0700

    first commit

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/devq/mitech/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/devq/mitech/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
